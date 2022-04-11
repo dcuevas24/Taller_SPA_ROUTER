a0e7a03d13a0a81c5ec2bad1bba710284654779e
dcuevas24 <ivandaniel1224@gmail.com>
2022-04-11 15:46:35 -0500

    Hacer match entre la URL y una ruta

--- router.js
+++ router.js
@@ -4,6 +4,24 @@ class Router {
     this._loadInitialRoute();
   }
 
+  /*Hacer match URL con la Ruta (Unir URL a la que se quiere ir con la que nos
+  esta mostrando)*/
+  _matchUrlToRoute(urlSegs) {
+    const matchedRoute = this.routes.find((route) => {
+      const routePathSegs = route.path.split("/").slice(1);
+
+      if (routePathSegs.length !== urlSegs.length) {
+        return false;
+      }
+
+      return routePathSegs.every(
+        (routePathSeg, i) => routePathSeg === urlSegs[i]
+      );
+    });
+
+    return matchedRoute;
+  }
+
   //Cargar ruta inicial del proyecto (Donde esta parado el router)
   _loadInitialRoute() {
     const pathNameSplit = window.location.pathname.split("/");
